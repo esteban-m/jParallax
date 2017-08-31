2c1db97d25969730dd0d71fa9ab9a155925a1222
Esteban Mauvais <contact@esteban.bzh>
2017-08-31 22:07:44 +0200

    Mouette
    Ajout du fichier de type javascript de type plugin jQuery !

---
+++ jparallax.js
@@ -0,0 +1,20 @@
+(function($) {
+	$.fn.extend({
+		mouseParallax: function(opt) {
+			var defaults = { moveFactor: 5, zIndexValue: "-1", targetContainer: 'body' },
+				o = $.extend(defaults, opt);
+			return this.each(function() {
+				var background = $(this);
+				$(o.targetContainer).on('mousemove', function(e){
+					mouseX = e.pageX;
+					mouseY = e.pageY;
+					windowWidth = $(window).width();
+					windowHeight = $(window).height();
+					percentX = (0-((mouseX/windowWidth)*o.moveFactor) - (o.moveFactor/2)+o.moveFactor)/2;
+					percentY = (0-((mouseY/windowHeight)*o.moveFactor) - (o.moveFactor/2)+o.moveFactor)/2;
+					background[0].style.transform = "translate("+percentX+"%,"+percentY+"%)";
+				});
+			});
+		}					
+	});
+}(jQuery));
